d0bac40b0c87e0e62730d22328044a1c62f2bb35
Praneet Dixit <65533196+PraneetDixit@users.noreply.github.com>
2021-05-11 15:48:45 +0530

    Update track.js

--- func/track.js
+++ func/track.js
@@ -4,21 +4,21 @@ exports.handler = async (event, context) => {
   const { ipAdd } = event.queryStringParameters;
   try {
     const req = await axios.get(`https://ipgeolocation.abstractapi.com/v1/?api_key=${process.env.API_KEY}&ip_address=${ipAdd}`);
-//     const res = {
-//       ip: req.ip_address,
-//       location : {
-//         lat: req.latitude,
-//         lng: req.longitude,
-//         city: req.city,
-//         state: req.region,
-//         country: req.country_code
-//       },
-//       timezone: req.timezone.gmt_offset,
-//       isp: req.connection.isp_name
-//     };
+    const res = {
+      ip: req.ip_address,
+      location : {
+        lat: req.latitude,
+        lng: req.longitude,
+        city: req.city,
+        state: req.region,
+        country: req.country_code
+      },
+      timezone: req.timezone.gmt_offset,
+      isp: req.connection.isp_name
+    };
     return {
       statusCode: 200,
-      body: JSON.stringify(req)
+      body: JSON.stringify(res)
     };
   } catch (error) {
     return {

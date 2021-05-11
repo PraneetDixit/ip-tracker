56a9af3664e56c4cf49d642e8a346f35c2901fb3
Praneet Dixit <65533196+PraneetDixit@users.noreply.github.com>
2021-05-11 13:53:03 +0530

    Update track.js

--- netlify/functions/track.js
+++ netlify/functions/track.js
@@ -1,6 +1,12 @@
+const http = require("http");
+
 exports.handler = async (event, context) => {
-  return {
-      statusCode: 200,
-      body: "Hi"
+  const options = {
+    hostname: 'https://geo.ipify.org/',
+    port: 443,
+    path: '/api/v1?apiKey=at_CY48XydVQZP15tmrwYEgdGtWTsMLQ&ipAddress=8.8.8.8',
+    method: 'GET'
   }
+  let req = await http.request(options);
+  return req;
 }

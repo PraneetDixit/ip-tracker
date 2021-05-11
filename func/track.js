786359347495d08f3e0ecd07366fd7133c84071d
Praneet Dixit <65533196+PraneetDixit@users.noreply.github.com>
2021-05-11 16:15:53 +0530

    Update track.js

--- func/track.js
+++ func/track.js
@@ -1,29 +1,34 @@
 const axios = require("axios");
 
 exports.handler = async (event, context) => {
-  const { ipAdd } = event.queryStringParameters;
-  try {
-    const { data } = await axios.get(`https://ipgeolocation.abstractapi.com/v1/?api_key=${process.env.API_KEY}&ip_address=${ipAdd}`);
-    const res = {
-      ip: data.ip_address,
-      location : {
-        lat: data.latitude,
-        lng: data.longitude,
-        city: data.city ? data.city : "",
-        state: data.region ? data.region : "",
-        country: data.country_code
-      },
-      timezone: data.timezone.gmt_offset,
-      isp: data.connection.isp_name
-    };
-    return {
-      statusCode: 200,
-      body: JSON.stringify(res)
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: `Error: ${error.message}`
-    }
+//   const { ipAdd } = event.queryStringParameters;
+//   try {
+//     const { data } = await axios.get(`https://ipgeolocation.abstractapi.com/v1/?api_key=${process.env.API_KEY}&ip_address=${ipAdd}`);
+//     const res = {
+//       ip: data.ip_address,
+//       location : {
+//         lat: data.latitude,
+//         lng: data.longitude,
+//         city: data.city ? data.city : "",
+//         state: data.region ? data.region : "",
+//         country: data.country_code
+//       },
+//       timezone: data.timezone.gmt_offset,
+//       isp: data.connection.isp_name
+//     };
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify(res)
+//     };
+//   } catch (error) {
+//     return {
+//       statusCode: 500,
+//       body: `Error: ${error.message}`
+//     }
+//   }
+  
+  return{
+    statusCode: 200,
+    body: JSON.stringify(event.headers)
   }
 }

26d3b613e0d22f58aff336d13848b4fdfdeec440
Praneet Dixit <65533196+PraneetDixit@users.noreply.github.com>
2021-05-11 14:25:25 +0530

    Update track.js

--- netlify/functions/track.js
+++ netlify/functions/track.js
@@ -1,7 +1,7 @@
 const https = require("https");
 
 exports.handler = async (event, context) => {
-  https.get('https://geo.ipify.org/api/v1?apiKey=at_TEYGZ5THakrXcYjy5H5MZLVnbuFgl&ipAddress=1.1.1.1', (resp) => {
+  await https.get('https://geo.ipify.org/api/v1?apiKey=at_TEYGZ5THakrXcYjy5H5MZLVnbuFgl&ipAddress=1.1.1.1', (resp) => {
     let data = '';
 
     resp.on('data', (chunk) => {
